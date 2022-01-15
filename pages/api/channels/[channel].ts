f7e0974d10a89dc51ab76db4398184cf0e577c50
gaaasp <gaspard.lasorne@protonmail.com>
2022-01-15 15:36:29 +0100

    Add channel cache

--- pages/api/channels/[channel].ts
+++ pages/api/channels/[channel].ts
@@ -1,13 +1,17 @@
 import { getXMLChannel } from "lib";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	getXMLChannel(`${req.query.channel}`)
-		.then((text) => {
-			res.send(text);
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).send("UNKNOWN_ERROR");
-		});
-};
+let CHANNELS = {};
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+	CHANNELS[req.query.channel?.toString()]
+		? res.status(200).send(CHANNELS[req.query.channel?.toString()])
+		: getXMLChannel(`${req.query.channel}`)
+				.then((text) => {
+					CHANNELS[req.query.channel?.toString()] = text;
+					res.status(200).send(text);
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(500).send("UNKNOWN_ERROR");
+				});
